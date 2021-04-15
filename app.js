f2ac782168ac84642d557b770f76b8cf7b637af7
sim567 <kambojsimran44@gmail.com>
2021-04-15 20:18:51 +0530

    port change

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use((err,req,res,next) =>{
     res.status(statusCode).render('error',{err});
 });
 //process.env||
-const port =  3000
+const port = process.env.PORT || 3000
 app.listen(port,()=>
 {
     console.log(`serving on port ${port}`)
