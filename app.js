c830d8806cd89e0e976f395cd04aaf5bc121776d
sim567 <kambojsimran44@gmail.com>
2021-04-15 19:53:44 +0530

    change db_url

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
  
 //const dbURL= process.env.DB_URL;
 //process.env.DB_URL||
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 //'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl,{
      
